aaddbeabae0fc7117e0a9e07c2c607ef68e03159
40794505MonHtooAung <40794505@live.napier.ac.uk>
2025-10-02 13:26:56 +0630

    Testing Connection

--- src/main/java/com/napier/gp8/App.java
+++ src/main/java/com/napier/gp8/App.java
@@ -77,6 +77,7 @@ public class App {
     public static void main(String[] args) {
         App a = new App();
         a.connect();
+        System.out.println("Connection testing");
         a.disconnect();
     }
 }
