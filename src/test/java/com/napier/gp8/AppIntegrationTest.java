3b3d062a42de98eda36d3e63bbf757b42379a588
KhinChawShweYee <40794538@live.napier.ac.uk>
2025-11-12 13:09:02 +0630

    Update Integration Test

--- src/test/java/com/napier/gp8/AppIntegrationTest.java
+++ src/test/java/com/napier/gp8/AppIntegrationTest.java
@@ -3,8 +3,6 @@ package com.napier.gp8;
 import org.junit.jupiter.api.*;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
